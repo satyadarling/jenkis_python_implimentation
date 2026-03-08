f7b41573f8b374b97d8c83298d4e55dca4f12af0
satyadarling <60965529+satyadarling@users.noreply.github.com>
2026-03-08 13:00:16 +0530

    Add headless Chrome options to setup fixture

--- conftest.py
+++ conftest.py
@@ -7,7 +7,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture()
 def setup():
     options = Options()
+
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--remote-debugging-port=9222")
 
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()),
