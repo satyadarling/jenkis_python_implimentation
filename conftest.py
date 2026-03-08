a4c89386481ebdc8fa4c8494d7b90ab018041048
satyadarling <60965529+satyadarling@users.noreply.github.com>
2026-03-08 12:38:38 +0530

    Modify setup fixture for headless Chrome execution
    Updated the setup fixture to run Chrome in headless mode and changed driver.close() to driver.quit().

--- conftest.py
+++ conftest.py
@@ -1,11 +1,19 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+
 @pytest.fixture()
 def setup():
-   driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-   driver.get("https://rahulshettyacademy.com/loginpagePractise/")
-   yield driver
-   driver.close()
+    options = Options()
+    options.add_argument("--headless")
+
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
+
+    driver.get("https://rahulshettyacademy.com/loginpagePractise/")
+    yield driver
+    driver.quit()
